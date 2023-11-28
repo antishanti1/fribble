408b12697e9868016217895b53284aa22a4fcf08
antishanti1 <shutkova2603@gmail.com>
2023-11-28 16:10:32 -0500

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,47 @@
 import React from 'react'
+import Image from 'next/image'
+import { footerLinks } from '@/constant';
+
+type ColumnProps = {
+    title: string;
+    links: Array<string>;
+}
+
+const FooterColumn = ({title, links}:
+    ColumnProps) => {
+    return (
+        <div className='footer_column'>
+            <h4 className='font-semibold'>Title</h4>
+            <ul className='flex flex-xol gap-2 font-normal'>
+                Links
+            </ul>
+        </div>
+    );
+}
 
 const Footer = () => {
   return (
-    <div>
-      
+<footer className='flexStart footer'>
+    <div className='flex flex-col gap-12 w-full'>
+        <div className='flex items-start flex-col'>
+            <Image 
+            src='/logo.png'
+            width={115}
+            height={15}
+            alt='logo'
+            />
+
+        </div>
+        <p className='text-start text-sm font-normal mt-5 max-w-xs'>
+       Fribble is the world&apos;s leading community for creatives to share, grow, and get hired.
+       </p>
     </div>
+    <div className='flex flex-wrap gap-12'>
+        <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links}/>
+
+    </div>
+</footer>
+
   )
 }
 
